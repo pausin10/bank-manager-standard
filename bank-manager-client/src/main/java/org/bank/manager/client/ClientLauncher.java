ac4d4403dd485896aeaac41a75a816b6968c95c3
pausin10 <15iu0366@gmail.com>
2020-07-07 11:37:52 +0200

    Created initial application

---
+++ bank-manager-client/src/main/java/org/bank/manager/client/ClientLauncher.java
@@ -0,0 +1,15 @@
+package org.bank.manager.client;
+
+import com.ontimize.gui.ApplicationLauncher;
+import com.ontimize.gui.ApplicationToolBar;
+import com.ontimize.util.math.MathExpressionParserFactory;
+import com.ontimize.util.math.MathExpressionParser;
+
+public class ClientLauncher {
+   
+    public static void main(String[] args) {
+        ApplicationToolBar.DEFAULT_BUTTON_SIZE=30;
+        System.setProperty(MathExpressionParserFactory.MATH_EXPRESSION_PARSER_PROPERTY, MathExpressionParser.MESP);
+        ApplicationLauncher.main(args);
+    }
+}
