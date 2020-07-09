7fc5b6f3f5141c00fa53da637a4e68b93c1b784f
pausin10 <15iu0366@gmail.com>
2020-07-09 20:40:50 +0200

    Exercise 23

--- bank-manager-server/src/main/java/org/bank/manager/server/ProjectServerLauncher.java
+++ bank-manager-server/src/main/java/org/bank/manager/server/ProjectServerLauncher.java
@@ -1,20 +1,21 @@
 package org.bank.manager.server;
 
 import com.ontimize.db.DatabaseConnectionManager;
+import com.ontimize.db.SQLStatementBuilder;
+import com.ontimize.db.SQLStatementBuilder.ExtendedSQLConditionValuesProcessor;
 import com.ontimize.gui.ServerLauncher;
 
 public class ProjectServerLauncher {
-	
-	public static void main(String[] args) {
-		
-		DatabaseConnectionManager.offset = 5;
-	    DatabaseConnectionManager.offsetString = "quickstart";
-
-		try {
-			ServerLauncher.main(args);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
+    public static void main(String[] args) {
+        DatabaseConnectionManager.offset = 5;
+        DatabaseConnectionManager.offsetString = "quickstart";
+        ExtendedSQLConditionValuesProcessor exprProcessor = new ExtendedSQLConditionValuesProcessor();
+        // SQLStatementBuilder.setSQLConditionValuesProcessor(exprProcessor);
+        SQLStatementBuilder.getSQLStatementHandler(SQLStatementBuilder.HSQLDB_HANDLER).setSQLConditionValuesProcessor(exprProcessor);
+        try {
+            ServerLauncher.main(args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- bank-manager-server/src/main/java/org/bank/manager/server/entities/EAccounts.java
+++ bank-manager-server/src/main/java/org/bank/manager/server/entities/EAccounts.java
@@ -1,11 +1,17 @@
 package org.bank.manager.server.entities;
 
 import java.sql.Connection;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Hashtable;
 import java.util.Vector;
 import org.bank.manager.server.InsertUserDataEntity;
 import com.ontimize.db.DatabaseConnectionManager;
 import com.ontimize.db.EntityResult;
+import com.ontimize.db.SQLStatementBuilder.BasicExpression;
+import com.ontimize.db.SQLStatementBuilder.BasicField;
+import com.ontimize.db.SQLStatementBuilder.BasicOperator;
+import com.ontimize.db.SQLStatementBuilder.ExtendedSQLConditionValuesProcessor;
 import com.ontimize.locator.EntityReferenceLocator;
 
 
@@ -17,7 +23,28 @@ public class EAccounts extends InsertUserDataEntity {
 
     @Override
     public EntityResult query(Hashtable keysValues, Vector attributes, int sessionId, Connection con) throws Exception {
+        // Insert condition for ( (STARTDATE > 01/01/<current_year>) OR (ENDDATE
+        // is null or ENDDATE= ‘’) )
+
+        Calendar cal = Calendar.getInstance();
+        Date now = new Date();
+        cal.setTime(now);
+        cal.set(cal.get(Calendar.YEAR), 0, 1, 0, 0, 0);
+
+        BasicField startDateField = new BasicField("STARTDATE");
+        BasicField endDateField = new BasicField("ENDDATE");
+
+        BasicExpression be1 = new BasicExpression(startDateField, BasicOperator.MORE_EQUAL_OP, cal.getTime());
+        BasicExpression be2 = new BasicExpression(endDateField, BasicOperator.NULL_OP, null);
+        BasicExpression be3 = new BasicExpression(endDateField, BasicOperator.EQUAL_OP, "");
+        BasicExpression be2OR3 = new BasicExpression(be2, BasicOperator.OR_OP, be3);
+        BasicExpression be1OR2OR3 = new BasicExpression(be1, BasicOperator.OR_OP, be2OR3);
+
+        keysValues.put(ExtendedSQLConditionValuesProcessor.EXPRESSION_KEY, be1OR2OR3);
+
         System.out.println("EAccounts: Executing query.");
         return super.query(keysValues, attributes, sessionId, con);
     }
+    
+    
 }
